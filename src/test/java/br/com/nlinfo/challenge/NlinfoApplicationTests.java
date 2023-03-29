ed4e5e255ace34a17bb4d9df299629204f990a7c
vandersonmauricio <vandermau678@gmail.com>
2023-03-29 11:21:20 -0300

    ajustando erros no export

--- src/test/java/br/com/nlinfo/challenge/NlinfoApplicationTests.java
+++ src/test/java/br/com/nlinfo/challenge/NlinfoApplicationTests.java
@@ -1,11 +1,15 @@
 package br.com.nlinfo.challenge;
 
+import br.com.nlinfo.challenge.api.repository.TarefaRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class NlinfoApplicationTests {
 
+    @Autowired
+    private TarefaRepository tarefaRepository;
     @Test
     void contextLoads() {
     }
